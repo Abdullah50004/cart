37698affe96440d2e392174f4b901916298e55a5
myusername <abdullah1234550004@gmail.com>
2023-04-16 05:53:07 +0300

    'added'

---
+++ lib/constant/image_asset.dart
@@ -0,0 +1,5 @@
+class imageAsset {
+  static const rootImages = 'images';
+  static const gasdetailes_one = '$rootImages/m.png';
+  static const gasdetailes_two = '$rootImages/s.jpg';
+}
---
+++ lib/model/detalies_model.dart
@@ -0,0 +1,6 @@
+class Detaliesmodel {
+  final String? title;
+  final String? image;
+
+  Detaliesmodel(this.title, this.image);
+}
